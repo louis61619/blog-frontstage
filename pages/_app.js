7c704643927edd24eae741d298ff3a1998065058
louis61619 <louis61619@gmail.com>
2021-04-04 12:26:41 +0800

    修復 pwa

--- pages/_app.js
+++ pages/_app.js
@@ -1,45 +1,65 @@
-// import React, { memo, useEffect, useState,  } from "react";
-import { useEffect } from 'react'
-import ReactDOM from 'react-dom';
-import { Provider, useDispatch } from 'react-redux'
-import { withRouter, useRouter } from 'next/router'
+import React, { Fragment } from "react";
+import Head from "next/head";
+import { useEffect } from "react";
+import ReactDOM from "react-dom";
+import { Provider, useDispatch } from "react-redux";
+import { withRouter, useRouter } from "next/router";
 import Router from "next/router";
 import NProgress from "nprogress";
 
-import { useStore } from '~/store'
-import { useLoadingSkeleton } from '~/utils/custom-hook'
+import { useStore } from "~/store";
+import { useLoadingSkeleton } from "~/utils/custom-hook";
 
-import 'antd/dist/antd.css'
-import '../styles/globals.css'
-import 'moment/locale/zh-tw'
+import "antd/dist/antd.css";
+import "../styles/globals.css";
+import "moment/locale/zh-tw";
 import "../styles/progress.css";
 
 import Header from "~/components/common/header";
-import Footer from '~/components/common/footer'
-import Auth from '~/components/common/auth'
+import Footer from "~/components/common/footer";
+import Auth from "~/components/common/auth";
 
-Router.onRouteChangeStart = url => {
+Router.onRouteChangeStart = (url) => {
   NProgress.start();
 };
 Router.onRouteChangeComplete = () => NProgress.done();
 Router.onRouteChangeError = () => NProgress.done();
 
-
 function MyApp({ Component, pageProps }) {
-  const store = useStore(pageProps?.initialReduxState)
-  const router = useRouter()
+  const store = useStore(pageProps?.initialReduxState);
+  const router = useRouter();
 
   return (
-    <Provider store={store}>
-      <Header />
+    <>
+      <Head>
+        <meta charSet="utf-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+
+        <link rel="manifest" href="/manifest.json" />
+        <link
+          href="/icon-192x192.png"
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
+      </Head>
+      <Provider store={store}>
+        <Header />
         <Auth>
           <Component {...pageProps} key={router.asPath} />
-        </Auth> 
-      {/* <Footer /> */}
-    </Provider>
-  )
+        </Auth>
+        {/* <Footer /> */}
+      </Provider>
+    </>
+  );
 }
 
-export default MyApp
-
-
+export default MyApp;
